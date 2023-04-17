14e880328732c6c3db457e84446ca6bcff858e3c
Roberto <robertorebolledo151@gmail.com>
2023-04-17 13:32:08 +0200

    creacion del paquete del proyecto

---
+++ BichoPedia/src/main/java/com/salesianostriana/dam/bichopedia/BichoPediaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.bichopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BichoPediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BichoPediaApplication.class, args);
+	}
+
+}
---
+++ BichoPedia/src/test/java/com/salesianostriana/dam/bichopedia/BichoPediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.bichopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichoPediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
