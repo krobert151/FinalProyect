83cb6f575cd8f09d70e70e0f3ef1e0d1766f9423
Roberto <robertorebolledo151@gmail.com>
2023-04-18 12:57:32 +0200

    cambios

---
+++ BichoPedia/src/main/java/com/salesianostriana/dam/model/Producto.java
@@ -0,0 +1,29 @@
+package com.salesianostriana.dam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="producto")
+public class Producto {
+	@Id
+	@GeneratedValue
+	private Long idProducto;
+	private String nombre;
+	private double precioU;
+	private String descripcion;
+	private int valoracion;
+	private String categoria;
+	
+}
