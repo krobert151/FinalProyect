39e32ebb78af8b26eaf7da1dc8cdd9f5b05f5cdf
Roberto <robertorebolledo151@gmail.com>
2023-05-22 13:28:29 +0200

    cambios menores

--- BichoPedia/src/main/java/com/salesianostriana/dam/bichopedia/BichoPediaApplication.java
+++ BichoPedia/src/main/java/com/salesianostriana/dam/bichopedia/BichoPediaApplication.java
@@ -1,8 +1,10 @@
 package com.salesianostriana.dam.bichopedia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class BichoPediaApplication {
 
